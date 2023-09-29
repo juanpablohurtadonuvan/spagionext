34346d2f952dc46f1c2b4a6f8c5ba9ba05883a4c
Juan Pablo Hurtado <juanpablohurtadonuvan@gmail.com>
2023-09-29 11:08:12 -0500

    feat: footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,48 +9,11 @@ function Footer() {
         <img src="/a.png" className="mr-4 max-h-20" alt="Solana" />{" "}
         {/* Añade margen a la derecha */}
       </div>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <div className="col-span-1">
-          <h2 className="mb-4 text-xl font-bold">Enlaces rápidos</h2>
-          <ul>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Menú
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Reservas
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Contacto
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-span-1">
-          <h2 className="mb-4 text-xl font-bold">Horario de apertura</h2>
-          <p>Lunes - Viernes: 11:00 AM - 10:00 PM</p>
-          <p>Sábado - Domingo: 12:00 PM - 11:00 PM</p>
-        </div>
-        <div className="col-span-1">
-          <h2 className="mb-4 text-xl font-bold">Contacto</h2>
-          <p>Dirección: 123 Calle Principal</p>
-          <p>Teléfono: (123) 456-7890</p>
-          <p>Email: info@spagio.com</p>
-        </div>
-      </div>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"></div>
       <div className="flex items-center justify-center mt-4">
         {" "}
         {/* Añade margen superior */}
-        Spagio - Copyright © 2023 derechos reservados
+         © 2023
       </div>
     </footer>
   );
